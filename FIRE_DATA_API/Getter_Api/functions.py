4b98df53cf816e34d22ac500c48fe9ae925e926c
tactnician <tactnician@gmail.com>
2019-10-20 13:09:35 -0400

    added basic functions for getting nasa firms data and open weather data

--- FIRE_DATA_API/Getter_Api/functions.py
+++ FIRE_DATA_API/Getter_Api/functions.py
@@ -4,13 +4,36 @@ import os
 import requests
 import json 
 
-# URLs 
-lance_firms_wget = 'wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=4 "https://nrt4.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS" --header "Authorization: Bearer C751EA24-F34E-11E9-9D0F-ABF3207B60E0" -P ../../'
+# API Tokens 
+nasa_lance_token = 'C751EA24-F34E-11E9-9D0F-ABF3207B60E0'
+open_weather_token = ''
+
 
 # Functions 
-def get_firms():
-    os.system(lance_firms_wget)
+
+def get_lon():
     pass
 
-def get_weather():
+def get_lat():
     pass
+
+def get_firms():
+    lance_firms_url = 'https://nrt4.modaps.eosdis.nasa.gov/api/v2/content/archives/FIRMS'
+    lance_firms_wget = f'wget -e robots=off -m -np -R .html,.tmp -nH --cut-dirs=4 "{lance_firms_url}" --header "Authorization: Bearer {nasa_lance_token}" -P ../../'
+    return os.system(lance_firms_wget)
+
+def get_weather(lon, lat):
+    open_weather_url = f'api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon} '
+    response = requests.get(open_weather_url)
+    if response.status_code == 200:
+        return json.loads(response.content.decode('utf-8'))
+    else:
+        return None
+
+
+## Notes 
+
+# prob need a function to check if  user input is within an already checked radius
+# so as not to exceed request limit of Open weather data. 
+
+# open weather has a retangular box api call that may solve this problem 
