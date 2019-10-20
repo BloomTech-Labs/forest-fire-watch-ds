5d6e04753eb42989dfc6718a02a476aeb0efba84
tactnician <tactnician@gmail.com>
2019-10-20 16:13:37 -0400

    added model for open weather api

--- FIRE_DATA_API/Getter_Api/models.py
+++ FIRE_DATA_API/Getter_Api/models.py
@@ -13,3 +13,25 @@ class Active_Fire(db.Model):
     def __repr__(self):
         return "<lat/long/fire/timestamp {},{}, {}, {}>".format(self.latitude, self.longitude, 
         self.fire, self.timest)
+
+class Weather_Curr(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.VARCHAR(7))
+    longitude = db.Column(db.VARCHAR(7))
+    wind_speed = db.Column()
+    wind_deg = db.Column()
+    clouds = db.Column()
+    rain_1h = db.Column()
+    rain_3h = db.Column()
+    snow_1h = db.Column()
+    snow_3h = db.Column()
+
+    def __repr__(self):
+        return ("<lat/long/wind_speed/wind_deg/clouds/rain_1h/rain_3h/snow_1h/snow_3h {}, {}, {} {}, {}, {}, {}, {}, {}>".format(
+            self.latitude, self.longitude, self.timestamp self.wind_speed, self.wind_deg, 
+            self.clouds, self.rain_1h, self.rain_3h, self.snow_1h, self.snow_3h ))
+
+
+
+        
+
